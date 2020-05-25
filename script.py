0e3fcd6f702af4e67b07853164de1c631befee64
ElenaSyvokaite <elenasyvokaite@gmail.com>
2020-05-25 21:05:35 +0100

    list comprehension and data frame

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
+import pandas as pd
 
 my_url = 'https://www.waterstones.com/category/fiction/classic-fiction/page/1e'
 # opens connection, grabs the page
@@ -15,14 +16,18 @@ page_soup = soup(page_html, "html.parser")
 bookInfo = page_soup.findAll("div", {"class":"info-wrap"})
 print(len(bookInfo)) # prints 24 
 
-# parsing 
+# testing parsing for 1 item
+bookTitle = bookInfo[0].div.a.get_text() # prints The Plague
+bookAuthor = bookInfo[0].span.get_text() # prints Albert Camus 
 
-# parsing info from 1 item
-container = bookInfo[0]
+# list comprehension for items in bookInfo
+bookTitles = [item.div.a.get_text() for item in bookInfo]
+bookAuthors = [item.span.get_text().strip() for item in bookInfo] 
 
-bookTitle = container.div.a.get_text() # prints The Plague
-print(bookTitle) 
-
-bookAuthor = container.span.get_text() # prints Albert Camus 
-print(bookAuthor) 
-
+# tidies info into table
+bookStuff = pd.DataFrame(
+    {
+        'Book_title': bookTitles,
+        'Book_author': bookAuthors,
+    })
+print(bookStuff)
